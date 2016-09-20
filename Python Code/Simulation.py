ff3364e3fbde558006c27fc56b2b379155d1daa8
John Gilbertson <ja.gilbertson1@gmail.com>
2016-09-20 22:50:10 +1000

    Simulation reads in dynamic times and is set up to also run for dynamic schedule. Just need to figure out how to nicely find dynamic arrival times

--- Python Code/Simulation.py
+++ Python Code/Simulation.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import pandas as pd
 
 mu = 1
 N = 15
@@ -7,56 +8,76 @@ gamma = 0.5
 
 numRuns = 100000
 
-# store total waiting time, total service time, total idle time and total cost for each run
-totalWaitTime = [0]*numRuns
-totalServiceTime = [0] * numRuns
-totalIdleTime = [0]*numRuns
-totalCost = [0]*numRuns
+# store arrival times, service start and end times for each customer, run and schedule
+arrivalTime = np.zeros(shape = (2,numRuns,N))
+serviceStart = np.zeros(shape = (2,numRuns,N))
+serviceEnd = np.zeros(shape = (2,numRuns,N))
+
+# store waiting time for each customer, run and schedule
+waitingTime = np.zeros(shape = (2,numRuns,N))
+
+# store total waiting time, total service time, total idle time and total cost for each run and schedule
+totalWaitTime = np.zeros(shape = (2,numRuns))
+totalServiceTime = np.zeros(shape = (2,numRuns))
+totalIdleTime = np.zeros(shape = (2,numRuns))
+totalCost = np.zeros(shape = (2,numRuns))
+
+# expected cost of each schedule
+cost = np.zeros(shape = 2)
 
 # read in static interarrival times
 with open("Static_Output.csv", 'r') as outputFile:
     reader = csv.reader(outputFile)
     for row in reader:
         if (row[0:3] == [str(N),str(gamma),str(mu)]):
-            interarrival = [float(val) for val in row[3:N+2]]
-            ExpCost = float(row[-1])
+            interarrivalStatic = [float(val) for val in row[3:N+2]]
+            cost[0] = float(row[-1])
             break
 
-# convert to arrival time of all N customers
-arrivalTime = np.append(0, np.cumsum(interarrival))
+# read in dynamic interarrival times
+interarrivalDynamic = dict()
 
+with open("Dynamic_Output.csv", 'r') as outputFile:
+    reader = csv.reader(outputFile)
+    for row in reader:
+        if (row[2] == str(gamma) and row[3] == str(mu)):
+            if (int(row[0]) == N and int(row[1]) == 0):
+                interarrivalDynamic[(int(row[0]), int(row[1]))] = float(row[4])
+                cost[1] = float(row[5])
+            
+            elif (int(row[0]) + int(row[1]) <= N):
+                interarrivalDynamic[(int(row[0]), int(row[1]))] = float(row[4])        
+           
 # generate service time of all N customers (for all runs)
 serviceTime = np.random.exponential(scale = mu, size = (numRuns,N))
 
 for run in range(numRuns):
+    # calculate arrival time of all N customers for static schedule
+    arrivalTime[0,run] = np.append(0, np.cumsum(interarrivalStatic))    
+    
     # calculate time service starts and time service ends for each customer
-    serviceStart = [0]*N
-    serviceEnd = [0]*N
-
     # first customer
-    serviceStart[0] = arrivalTime[0]
-    serviceEnd[0] = serviceStart[0] + serviceTime[run][0]
+    serviceStart[0,run,0] = arrivalTime[0,run,0]
+    serviceEnd[0,run,0] = serviceStart[0,run,0] + serviceTime[run,0]
 
     # all other customers
     for i in range(1, N):
-        serviceStart[i] = max(arrivalTime[i], serviceEnd[i - 1])
-        serviceEnd[i] = serviceStart[i] + serviceTime[run][i]
+        serviceStart[0,run,i] = max(arrivalTime[0,run,i], serviceEnd[0,run,i-1])
+        serviceEnd[0,run,i] = serviceStart[0,run,i] + serviceTime[run,i]
 
     # calculate waiting time of each customer
-    waitingTime = [0]*N
-
     for i in range(N):
-        waitingTime[i] = serviceStart[i] - arrivalTime[i]
+        waitingTime[0,run,i] = serviceStart[0,run,i] - arrivalTime[0,run,i]
 
     # calculate total waiting time and total service time
-    totalWaitTime[run] = sum(waitingTime)
-    totalServiceTime[run] = serviceEnd[N - 1]
+    totalWaitTime[0,run] = sum(waitingTime[0,run])
+    totalServiceTime[0,run] = serviceEnd[0,run,N-1]
 
     # calculate total idle time
-    totalIdleTime[run] = totalServiceTime[run] - sum(serviceTime[run])
+    totalIdleTime[0,run] = totalServiceTime[0,run] - sum(serviceTime[run])
     
     # calculate total cost
-    totalCost[run] = gamma * totalServiceTime[run] + (1 - gamma) * totalWaitTime[run]
+    totalCost[0,run] = gamma * totalServiceTime[0,run] + (1 - gamma) * totalWaitTime[0,run]
 
-print('Expected Cost is ' + str(ExpCost))
-print('Mean Cost is ' + str(np.mean(totalCost)))
+print('Expected Cost is ' + str(cost[0]))
+print('Mean Cost is ' + str(np.mean(totalCost[0])))
