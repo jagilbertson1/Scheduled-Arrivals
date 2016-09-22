612d9bf7d55d68d9823ec73f23a14d7162d59c46
John Alexander Gilbertson <johnag@student.unimelb.edu.au>
2016-09-22 13:20:06 +1000

    Easy histograms in R. Need to use ggplot

--- R Code/Simulation_Graphs.R
+++ R Code/Simulation_Graphs.R
@@ -6,3 +6,8 @@ setwd("~/Documents/Scheduled-Arrivals/R\ Code")
 data = read.csv(file = "../Python\ Code/Simulation_Output.csv", header = TRUE, sep = ",")
 
 View(data)
+
+# histograms
+hist(data[data$schedule == 'static','Cost'], breaks = 50, freq = FALSE, xlim = c(0, 30))
+hist(data[data$schedule == 'dynamic','Cost'], breaks = 50, freq = FALSE, xlim = c(0, 30))
+hist(data[data$schedule == 'static','Cost'] - data[data$schedule == 'dynamic','Cost'], breaks = 100, freq = FALSE, xlim = c(-10, 10))
