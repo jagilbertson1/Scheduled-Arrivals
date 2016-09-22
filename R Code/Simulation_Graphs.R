53c854802a976e08c2075ddd434efd13ca3ec549
John Alexander Gilbertson <johnag@student.unimelb.edu.au>
2016-09-22 15:05:18 +1000

    Basic method to plot histograms using ggplot. Seems good, but probably don't want all these. Pick out the good ones. Also use 10^5 data to get better shape. Next step is to get line plots working. Don't worry too much about colour and background for now

--- R Code/Simulation_Graphs.R
+++ R Code/Simulation_Graphs.R
@@ -5,9 +5,65 @@ setwd("~/Documents/Scheduled-Arrivals/R\ Code")
 # import data
 data = read.csv(file = "../Python\ Code/Simulation_Output.csv", header = TRUE, sep = ",")
 
-View(data)
+# construct static data frame
+static = data[data$schedule == 'static',]
+static = static[order(static$run),]
 
-# histograms
-hist(data[data$schedule == 'static','Cost'], breaks = 50, freq = FALSE, xlim = c(0, 30))
-hist(data[data$schedule == 'dynamic','Cost'], breaks = 50, freq = FALSE, xlim = c(0, 30))
-hist(data[data$schedule == 'static','Cost'] - data[data$schedule == 'dynamic','Cost'], breaks = 100, freq = FALSE, xlim = c(-10, 10))
+# construct dynamic data frame
+dynamic = data[data$schedule == 'dynamic',]
+dynamic = dynamic[order(dynamic$run),]
+
+# construct difference data frame
+diff = static[,c(-1,-2)] - dynamic[,c(-1,-2)]
+
+# cost histograms
+ggplot(data = static, aes(static$Cost)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(5, 30, by = 0.5), col = "red", fill = "green", alpha = 0.2) + 
+  xlim(c(5, 30)) + ylim(c(0, 0.15))
+
+ggplot(data = dynamic, aes(dynamic$Cost)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(5, 30, by = 0.5), col = "red", fill = "green", alpha = 0.2) + 
+  xlim(c(5, 30)) + ylim(c(0, 0.15))
+
+ggplot(data = diff, aes(diff$Cost)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(-5, 10, by = 0.25), col = "red", fill = "green", alpha = 0.2) + 
+  xlim(c(-5, 10)) + ylim(c(0,0.1))
+
+# average customer waiting time histograms
+ggplot(data = static, aes(static$TWT/15)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(0, 2, by = 0.05), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(0, 2)) + ylim(c(0, 0.15))
+
+ggplot(data = dynamic, aes(dynamic$TWT/15)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(0, 2, by = 0.05), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(0, 2)) + ylim(c(0, 0.15))
+
+ggplot(data = diff, aes(diff$TWT/15)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(-0.5, 1, by = 0.025), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(-0.5, 1)) + ylim(c(0, 0.15))
+
+# total service time histograms
+ggplot(data = static, aes(static$TST)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(10, 35, by = 0.5), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(10, 35)) + ylim(c(0, 0.25))
+
+ggplot(data = dynamic, aes(dynamic$TST)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(10, 35, by = 0.5), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(10, 35)) + ylim(c(0, 0.25))
+
+ggplot(data = diff, aes(diff$TST)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(-5, 10, by = 0.5), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(-5, 10)) + ylim(c(0, 0.2))
+
+# total idle time histograms
+ggplot(data = static, aes(static$TIT)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(0, 20, by = 0.5), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(0, 20)) + ylim(c(0, 0.15))
+
+ggplot(data = dynamic, aes(dynamic$TIT)) + 
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(0, 20, by = 0.5), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(0, 20)) + ylim(c(0, 0.15))
+
+ggplot(data = diff, aes(diff$TIT)) +
+  geom_histogram(aes(y=..count../sum(..count..)), breaks = seq(-5, 10, by = 0.5), col = "red", fill = "green", alpha = 0.2) +
+  xlim(c(-5, 10)) + ylim(c(0, 0.2))
